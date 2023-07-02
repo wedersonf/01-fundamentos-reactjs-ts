73b90cf1251f9fc29e0c6fedcd692d16b28463b8
wedersonf <wedymf@hotmail.com>
2023-07-02 20:25:38 -0300

    add post type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Header } from "./components/Header"
-import { Post } from "./components/Post"
+import { Post, PostType } from "./components/Post"
 import { Sidebar } from "./components/Sidebar";
 
 import styles from './App.module.css';
 import './global.css'
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -48,10 +48,7 @@ function App() {
           {posts.map(post => {
             return (
               <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
